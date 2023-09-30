fbd620f46a41c4760e235b0eb96c9d2aefbf50c8
Adam Havlíček <havlicek.adam23@gmail.com>
2023-09-30 12:53:42 +0200

    feat: implemented loading words from file

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ pub fn Root(comptime T: type) type {
     return struct {
         const Self = @This();
 
+        const Slice = []const T;
+
         const ChildrenType = std.AutoHashMap(T, Node);
 
         allocator: std.mem.Allocator,
@@ -27,13 +29,8 @@ pub fn Root(comptime T: type) type {
                 };
             }
 
-            fn set_char(self: *Node, char: T) void {
-                self.data = char;
-            }
-
             fn set_end_of_word(self: *Node) void {
                 self.end_of_word = true;
-                std.log.debug("setting this bad boy to {}\n", .{self.end_of_word});
             }
         };
 
@@ -47,48 +44,39 @@ pub fn Root(comptime T: type) type {
             };
         }
 
-        pub fn add_word(self: *Self, value: []const u8) !void {
+        pub fn add_word(self: *Self, value: Slice) !void {
             var last_child: ?*Node = null;
             for (value) |char| {
-                if (last_child) |node| {
-                    var new_child = try Node.init(node.allocator);
-                    new_child.set_char(char);
+                var children = if (last_child) |node| node.children else self.children;
 
-                    try last_child.?.children.put(char, new_child);
-                    last_child = last_child.?.children.getPtr(char);
-                } else {
-                    if (!self.children.contains(char)) {
-                        var new_child = try Node.init(self.allocator);
-                        new_child.set_char(char);
+                if (!children.contains(char)) {
+                    var new_child = try Node.init(children.allocator);
+                    new_child.data = char;
 
-                        try self.children.put(char, new_child);
-                        last_child = self.children.getPtr(char);
-                    } else {
-                        last_child = self.children.getPtr(char);
-                    }
+                    try children.put(char, new_child);
                 }
+
+                last_child = children.getPtr(char);
             }
 
             last_child.?.*.set_end_of_word();
         }
 
-        pub fn exists(self: *Self, value: []const u8) bool {
-            var node: ?Node = null;
-            for (value, 0..value.len) |char, index| {
-                if (index == 0) {
-                    node = self.children.get(char);
-                    std.log.debug("{u}\n", .{node.?.data});
-                } else if (node == null) {
-                    break;
+        pub fn exists(self: *Self, value: Slice) bool {
+            if (value.len == 0) {
+                return false;
+            }
+
+            var node: ?Node = self.children.get(value[0]);
+            for (value[1..value.len]) |char| {
+                if (node) |node_| {
+                    node = node_.children.get(char);
                 } else {
-                    node = node.?.children.get(char);
-                    std.log.debug("{u}\n", .{node.?.data});
+                    break;
                 }
             }
 
             if (node) |node_| {
-                std.log.debug("last char is {u}\n", .{node_.data});
-                std.log.debug("is this bad boy true? {}\n", .{node_.end_of_word});
                 return node_.end_of_word;
             } else {
                 return false;
@@ -101,17 +89,37 @@ pub fn Root(comptime T: type) type {
     };
 }
 
+const CharRoot = Root(u8);
+
+pub fn loadWords(allocator: std.mem.Allocator, path: []const u8, root: *CharRoot) !void {
+    _ = allocator;
+    const file = try std.fs.openFileAbsolute(path, .{});
+    defer file.close();
+
+    var file_buffer = std.io.bufferedReader(file.reader());
+    var in_stream = file_buffer.reader();
+
+    var buf: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try root.add_word(line);
+    }
+}
+
 pub fn main() !void {
-    const word = "TESTER";
-    var logging = std.heap.loggingAllocator(std.heap.page_allocator);
-    var arena = std.heap.ArenaAllocator.init(logging.allocator());
+    // var logging = std.heap.loggingAllocator(std.heap.page_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
 
     const allocator = arena.allocator();
-    const CharRoot = Root(u8);
+
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const path = args[1];
+    const word = args[2];
 
     var root = try CharRoot.init(allocator);
-    try root.add_word(word);
+    try loadWords(allocator, path, &root);
 
     std.debug.print("{}\n", .{root.exists(word)});
 }
