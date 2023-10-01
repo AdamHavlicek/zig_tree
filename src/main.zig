441ff1695237eae4afc9be961bf8a6f85ce4dc29
Adam Havlíček <havlicek.adam23@gmail.com>
2023-10-01 14:17:00 +0200

    refactor: minor improvements

--- src/main.zig
+++ src/main.zig
@@ -8,22 +8,23 @@ pub fn Root(comptime T: type) type {
 
         const ChildrenType = std.AutoHashMap(T, Node);
 
-        allocator: *std.mem.Allocator,
+        allocator: std.mem.Allocator,
         children: *ChildrenType,
 
         const Node = struct {
-            allocator: *std.mem.Allocator,
+            allocator: std.mem.Allocator,
             data: T,
             end_of_word: bool,
             children: *ChildrenType,
 
-            fn init(allocator: *std.mem.Allocator) !Node {
+            fn init(allocator: std.mem.Allocator, data: T) !Node {
                 var children = try allocator.create(ChildrenType);
-                children.* = ChildrenType.init(allocator.*);
+                children.* = ChildrenType.init(allocator);
+
                 return .{
                     .allocator = allocator,
                     .end_of_word = false,
-                    .data = 0,
+                    .data = data,
                     .children = children,
                 };
             }
@@ -31,11 +32,12 @@ pub fn Root(comptime T: type) type {
             pub fn deinit(self: *Node) void {
                 var it = self.children.valueIterator();
                 while (it.next()) |value| {
-                    value.*.deinit();
+                    value.deinit();
                 }
 
-                self.children.*.deinit();
+                self.children.deinit();
                 self.allocator.destroy(self.children);
+                self.* = undefined;
             }
 
             fn set_end_of_word(self: *Node) void {
@@ -43,44 +45,46 @@ pub fn Root(comptime T: type) type {
             }
         };
 
-        pub fn init(allocator: *std.mem.Allocator) !Self {
+        pub fn init(allocator: std.mem.Allocator) !Self {
             var children = try allocator.create(ChildrenType);
-            children.* = ChildrenType.init(allocator.*);
+            children.* = ChildrenType.init(allocator);
+
             return .{
                 .allocator = allocator,
                 .children = children,
             };
         }
 
-        // Destructor to free allocated memory
         pub fn deinit(self: *Self) void {
             var it = self.children.valueIterator();
             while (it.next()) |value| {
-                value.*.deinit();
+                value.deinit();
             }
 
-            self.children.*.deinit();
+            self.children.deinit();
             self.allocator.destroy(self.children);
+            self.* = undefined;
         }
 
         pub fn add_word(self: *Self, value: Slice) !void {
-            var last_child: ?*Node = null;
-            for (value) |char| {
-                var children = if (last_child) |node| node.children else self.children;
-
-                if (!children.contains(char)) {
-                    var new_child = try Node.init(self.allocator);
-                    new_child.data = char;
-
-                    children.put(char, new_child) catch |err| {
-                        std.log.err("{}", .{err});
-                    };
-                }
-
-                last_child = children.getPtr(char);
+            if (value.len == 0) {
+                return;
             }
 
-            last_child.?.*.set_end_of_word();
+            var maybe_node_ptr: ?*Node = null;
+            for (value) |char| {
+                var children = if (maybe_node_ptr) |node| node.children else self.children;
+
+                if (!children.contains(char)) {
+                    const node = try Node.init(children.allocator, char);
+
+                    try children.put(char, node);
+                }
+
+                maybe_node_ptr = children.getPtr(char);
+            }
+
+            maybe_node_ptr.?.*.set_end_of_word();
         }
 
         pub fn exists(self: *Self, value: Slice) bool {
@@ -88,17 +92,17 @@ pub fn Root(comptime T: type) type {
                 return false;
             }
 
-            var node: ?Node = self.children.get(value[0]);
+            var maybe_node: ?Node = self.children.get(value[0]);
             for (value[1..value.len]) |char| {
-                if (node) |node_| {
-                    node = node_.children.get(char);
+                if (maybe_node) |node| {
+                    maybe_node = node.children.get(char);
                 } else {
                     break;
                 }
             }
 
-            if (node) |node_| {
-                return node_.end_of_word;
+            if (maybe_node) |node| {
+                return node.end_of_word;
             } else {
                 return false;
             }
@@ -150,7 +154,7 @@ test "[Root] - [Node]" {
     const word1 = "TEST";
     var allocator = std.testing.allocator;
 
-    var root = try CharRoot.init(&allocator);
+    var root = try CharRoot.init(allocator);
     defer root.deinit();
 
     try root.add_word(word);
